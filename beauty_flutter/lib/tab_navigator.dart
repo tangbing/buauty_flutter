453d59f66eb08b8d8bba51a563108eaa377f417c
Tb <tbit_144135@sina.com>
2021-11-05 00:01:37 +0800

    底部导航栏

---
+++ beauty_flutter/lib/tab_navigator.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+
+///底部导航栏框架搭建
+class TabNavigator extends StatefulWidget {
+  const TabNavigator({Key? key}) : super(key: key);
+
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+
+  final _defaultColor = Colors.grey;
+  final _activeColor  = Colors.blue;
+
+  int _selectIndex = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+             gradient: LinearGradient(
+               colors: [
+                 Color(0xffedeef0),
+                 Color(0xffe6e7e9)
+               ],
+               begin: Alignment.topCenter,
+               end: Alignment.bottomCenter
+             )
+          ),
+        child: Center(
+          child: Text("click $_selectIndex", style: TextStyle(fontSize: 33,color: Colors.red),),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectIndex,
+        items: [
+        _bottomNavigItem("本周", Icons.folder, 0),
+        _bottomNavigItem("本周", Icons.explore, 1),
+        _bottomNavigItem("本周", Icons.donut_small, 2),
+        _bottomNavigItem("本周", Icons.person, 3),
+      ],
+        onTap: (index) {
+          // 修改状态
+          setState(() {
+            _selectIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  _bottomNavigItem(String title, IconData icon, int index) {
+     return BottomNavigationBarItem(
+        icon: Icon(icon, color: _defaultColor),
+        activeIcon: Icon(icon, color: _activeColor),
+      title: Text(title, style: TextStyle(color: _selectIndex == index ? _activeColor : _defaultColor))
+    );
+  }
+}
